c122ab1d625a3d57850b31f4866145618a17506f
AmiHoppus182 <aiami@live.com>
2015-02-26 15:35:14 -0300

    Initial Commit

---
+++ tapTheLhama/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tapTheLhama
+//
+//  Created by Ami Garcia on 2/26/15.
+//  Copyright (c) 2015 Ami Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tapTheLhama/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tapTheLhama
+//
+//  Created by Ami Garcia on 2/26/15.
+//  Copyright (c) 2015 Ami Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
