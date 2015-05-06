f11ed8d91d73175f83d912129a04bf345f67d80f
AmiHoppus182 <aiami@live.com>
2015-05-06 16:14:45 -0300

    First Commit

---
+++ tapTheLhama/ViewControllerMenu.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerMenu.h
+//  tapTheLhama
+//
+//  Created by Ami Garcia on 3/2/15.
+//  Copyright (c) 2015 Ami Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerMenu : UIViewController
+
+@end
---
+++ tapTheLhama/ViewControllerMenu.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllerMenu.m
+//  tapTheLhama
+//
+//  Created by Ami Garcia on 3/2/15.
+//  Copyright (c) 2015 Ami Garcia. All rights reserved.
+//
+
+#import "ViewControllerMenu.h"
+
+@interface ViewControllerMenu ()
+
+@end
+
+@implementation ViewControllerMenu
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
